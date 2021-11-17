ba35cedde3dee1892c8dd470aff91ec543bb6d3b
Abi-RD <dame007abi@icloud.com>
2021-11-17 23:33:13 +0000

    more elif

--- conditionals_marks.py
+++ conditionals_marks.py
@@ -1,4 +1,35 @@
         # with elif
+
+# Write a program that determines the name of a shape from its number of sides. 
+# Read the number of sides from the user and then report the appropriate name as part of a meaningful message. 
+# Your program should support shapes with anywhere from 3 up to (and including) 10 sides. 
+# If a number of sides outside of this range is entered then your program should display an appropriate error message.
+
+nsides = int(input("Enter the number of side: "))
+name = " "
+
+if nsides == 3:
+    name = "Triangle"
+elif nsides == 4:
+    name = "Quadilateral"
+elif nsides == 5:
+    name = "Pentagon"
+elif nsides == 6:
+    name = "Hexgon"
+elif nsides == 7:
+    name = "Heptagon"
+elif nsides == 8:
+    name = "Octagon"
+elif nsides == 9:
+    name = "Nonagon"
+elif nsides == 10:
+    name = "Decagon"
+if name == " ":
+    print("Sorry," ,"Number sides not supported by this programme")
+else:
+    print("That's a", name)
+
+
 # mark = int(input("Enter you mark: "))
     
 """ if 84 < mark <= 100:
@@ -18,16 +49,15 @@ else:
 
             #without elif
 
-mark1 = int(input("Enter your marks: "))
+# mark1 = int(input("Enter your marks: "))
 
-if mark1 > 85:
+""" if mark1 > 85:
     print("Distinction!")
 else:
     if mark1 > 65 and mark1 < 85:
         print("Pass")
     else:
-        print("Fail")
-
+        print("Fail") """
 
 
 
