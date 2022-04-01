dc459fd5b403f2b15cfe98c908d15b8513fe93b3
Abi-RD <dame007abi@icloud.com>
2022-04-01 17:05:09 +0100

    adding SQL Challenge files

--- Sakila Questions n Solutions.sql
+++ Sakila Questions n Solutions.sql
@@ -90,5 +90,3 @@ SELECT category.`name`, count(film_category.film_id) FROM category
 JOIN film_category ON film_category.category_id=category.category_id
 GROUP BY film_category.category_id
 ORDER BY film_category.category_id LIMIT 1;
-
-
